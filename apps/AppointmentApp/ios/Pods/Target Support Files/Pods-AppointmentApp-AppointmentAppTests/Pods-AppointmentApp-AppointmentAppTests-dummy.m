c3c7e43187b5a4fc5ae44efc32c72bbf438194ac
Dinesh Khammakomati <craftnotion@Dineshs-Air.Dlink>
2020-09-22 00:12:39 +0530

    first commit

---
+++ apps/AppointmentApp/ios/Pods/Target Support Files/Pods-AppointmentApp-AppointmentAppTests/Pods-AppointmentApp-AppointmentAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppointmentApp_AppointmentAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_AppointmentApp_AppointmentAppTests
+@end
---
+++ apps/AppointmentApp/ios/Pods/Target Support Files/Pods-AppointmentApp-tvOS/Pods-AppointmentApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppointmentApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_AppointmentApp_tvOS
+@end
